a4de0bcc01103a24d5032a006c8104eec8dbbe5e
evsina <anastasiyaevsina@gmail.com>
2023-02-15 14:26:28 +0700

    HW 1

---
+++ lesson_1_task_2.py
@@ -0,0 +1,4 @@
+my_age = 29
+print(my_age)
+my_age = 32
+print(my_age)
---
+++ lesson_1_task_7.py
@@ -0,0 +1,15 @@
+def phone_number (a,b,c,d,e,f,g,h,i,j):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+    print(g)
+    print(h)
+    print(i)
+    print(j)
+    
+phone_number(8,8,0,0,5,5,5,3,5,3,5)
+
+
